ef3654088bd83eee66e148ed7eb6f3cd69daf52d
Ivan Plaksin <zero.netto.o@gmail.com>
2020-05-28 20:39:01 +0500

    Initial commit

---
+++ Assets/Scripts/Systems/GameLoader.cs
@@ -0,0 +1,33 @@
+﻿using Leopotam.Ecs;
+using UnityEngine;
+
+namespace Systems
+{
+    public class GameLoader : MonoBehaviour
+    {
+        private EcsWorld world;
+        private EcsSystems systems;
+
+        public void Start()
+        {
+            world = new EcsWorld();
+            
+            systems = new EcsSystems(world)
+                .ProcessInjects();
+            
+            systems.Init();
+        }
+
+        public void Update()
+        {
+            systems.Run();
+            //world.EndFrame();
+        }
+
+        public void OnDestroy()
+        {
+            systems.Destroy();
+            world.Destroy();
+        }
+    }
+}
