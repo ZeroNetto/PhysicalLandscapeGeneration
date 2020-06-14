6307a358e3382004514b4bd3f91c7d99eee942e4
Ivan Plaksin <zero.netto.o@gmail.com>
2020-06-14 12:53:51 +0500

    [fix] part of files was ignored in prev. commits.

---
+++ Assets/Editor/MeshEditor.cs
@@ -0,0 +1,34 @@
+﻿using Systems;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor (typeof (GameLoader))]
+public class MeshEditor : Editor {
+
+    private GameLoader gameLoader;
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Generate Mesh"))
+            gameLoader.Generate();
+
+        string numIterationsString = gameLoader.ErosionIterationsCount.ToString();
+        if (gameLoader.ErosionIterationsCount >= 1000) {
+            numIterationsString = (gameLoader.ErosionIterationsCount/1000) + "k";
+        }
+
+        if (GUILayout.Button ("Erode (" + numIterationsString + " iterations)"))
+            gameLoader.Erode();
+    }
+
+    void OnEnable () {
+        gameLoader = (GameLoader) target;
+        Tools.hidden = true;
+    }
+
+    void OnDisable () {
+        Tools.hidden = false;
+    }
+}
